eb289754b5dc49e0f9ed28af39eeb03b0e4cb08d
James Hund <24129429@sun.ac.za>
2020-09-16 17:54:02 +0200

    Method decalrations in warrior class

--- src/game/Warrior.java
+++ src/game/Warrior.java
@@ -8,5 +8,80 @@ public class Warrior{
 	public Warrior() {
 		
 	}
+
+	public void backupValues() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void decrementWarriorInvisibility() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setWarriorInvisibility(int val) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setInTrance() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setCuredFromTrance() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setSpecialAbilityPerformed() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void incrementAge() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setSpecialAbilityCount(int val) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void decrementSpecialAbilityCount() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void adjustDefenseStrength(double val) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void adjustOffensePower(double val) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void updateDefenseStrength() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setDefenseStrength(double val) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setOffensePowerStrength(double val) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void adjustHealth(double val) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
--- src/game/WarriorTypeInterface.java
+++ src/game/WarriorTypeInterface.java
@@ -4,4 +4,34 @@ public interface WarriorTypeInterface {
     void performSpecialAbility();
     void specialAbilityCompleted();
 
+    public void backupValues();
+    
+    public void decrementWarriorInvisibility();
+    
+    public void setWarriorInvisibility(int val);
+    
+    public void setInTrance();
+    
+    public void setCuredFromTrance();
+    
+    public void setSpecialAbilityPerformed();
+    
+    public void incrementAge();
+    
+    public void setSpecialAbilityCount(int val);
+    
+    public void decrementSpecialAbilityCount();
+    
+    public void adjustDefenseStrength(double val);
+    
+    public void adjustOffensePower(double val);
+    
+    public void updateDefenseStrength();
+    
+    public void setDefenseStrength(double val);
+    
+    public void setOffensePowerStrength(double val);
+    
+    public void adjustHealth(double val);
+    
 }
