1a7a59a65fd859467c2c239d81d2301cdcb57820
james <24129429@sun.ac.za>
2020-10-11 19:12:08 +0200

    Removed any testing things from bin folder

--- test.sh
+++ test.sh
@@ -3,12 +3,12 @@ cd bin;
 for i in {1..9}
 do
   echo -e "\nTesting test case $i\n";
-  java Game_24129429 "samples1/input0$i.txt" "1" | diff -s - samples1/outputVisualPlain/output0$i.txt --color;
+  java Game_24129429 "../samples1/input0$i.txt" "1" | diff -s - ../samples1/outputVisualPlain/output0$i.txt --color;
 done
 for i in {10..18}
 do
   echo -e "\nTesting test case $i\n";
-  java Game_24129429 samples1/input$i.txt 1 | diff -s - samples1/outputVisualPlain/output$i.txt --color;
+  java Game_24129429 ../samples1/input$i.txt 1 | diff -s - ../samples1/outputVisualPlain/output$i.txt --color;
 done
 echo "\n______________\nperforming demo tests\n______________\n";
 for i in {1..9}
