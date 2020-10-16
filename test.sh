470deceab43377d75480922d825ea973ef36f286
james <24129429@sun.ac.za>
2020-10-16 11:07:03 +0200

    Updated test.sh and added updated samples

--- test.sh
+++ test.sh
@@ -1,29 +1,25 @@
 #!/bin/bash
-empty=$("");
 cd bin;
+FAILED_CASES="Failed Cases: ";
 for i in {1..9}
 do
-  OUTPUT=$(java Game_24129429 "../samples1/input0$i.txt" "1" | diff - ../samples1/outputVisualPlain/output0$i.txt --color);
-  echo $OUTPUT > file.txt
-  if [ $OUTPUT != $empty ]
+  OUTPUT=$(java Game_24129429 "../samples2/input0$i.txt" "1" | diff - ../samples2/outputVisual/plain/output0$i.txt --color);
+  if [ ${#OUTPUT} != 0 ]
   then
   	echo -e "\nTesting test case $i\n";
-	echo $OUTPUT;
+	java Game_24129429 "../samples2/input0$i.txt" "1" | diff - ../samples2/outputVisual/plain/output0$i.txt --color;
+	FAILED_CASES +=" " + $i + " ";
   fi
 done
-for i in {10..23}
+for i in {10..33}
 do
-  echo -e "\nTesting test case $i\n";
-  java Game_24129429 ../samples1/input$i.txt 1 | diff - ../samples1/outputVisualPlain/output$i.txt --color;
+
+  OUTPUT=$(java Game_24129429 "../samples2/input$i.txt" "1" | diff - ../samples2/outputVisual/plain/output$i.txt --color);
+  if [ ${#OUTPUT} != 0 ]
+  then
+    echo -e "\nTesting test case $i\n";
+    java Game_24129429 ../samples2/input$i.txt 1 | diff - ../samples2/outputVisual/plain/output$i.txt --color;
+    FAILED_CASES+=" "$i" ";
+  fi
 done
-echo -e "\n______________\nperforming demo tests\n______________\n";
-for i in {1..9}
-do
-  echo -e "\nDemo test case $i\n";
-  java Game_24129429 ../testsFirstDemo/input0$i.txt 1 | diff - ../testsFirstDemo/outputVisual/plain/output0$i.txt --color;
-done
-for i in {10..25}
-do
-  echo -e "\nDemo test case $i\n";
-  java Game_24129429 ../testsFirstDemo/input$i.txt 1 | diff - ../testsFirstDemo/outputVisual/plain/output$i.txt --color;
-done
+echo $FAILED_CASES
